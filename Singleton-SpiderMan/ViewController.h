cf1cd9c3de5664e3ef4f3b14096be9d4f900383e
Anchlate Lee <anchlate@yahoo.com>
2016-05-01 14:16:25 +0800

    Initial Commit

---
+++ Singleton-SpiderMan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Singleton-SpiderMan
+//
+//  Created by Anchlate Lee on 16/5/1.
+//  Copyright © 2016年 Anchlate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
