5ccb42aa8bbdce955f8cdeefccfaa01654303622
Anchlate Lee <anchlate@yahoo.com>
2016-05-01 15:59:48 +0800

    sd

---
+++ Singleton-SpiderMan/SpiderMan.m
@@ -0,0 +1,13 @@
+//
+//  SpiderMan.m
+//  Singleton-SpiderMan
+//
+//  Created by Anchlate Lee on 16/5/1.
+//  Copyright © 2016年 Anchlate. All rights reserved.
+//
+
+#import "SpiderMan.h"
+
+@implementation SpiderMan
+
+@end
